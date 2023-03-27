b702eb362c83fea2639127373b0a90bbda691b51
hyetigran <tigran.dev@protonmail.com>
2023-03-27 13:54:48 -0400

    add chakra provider

---
+++ src/theme/index.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'IBM Plex Mono', sans-serif`,
+    body: `'IBM Plex Mono', 'Raleway', sans-serif`,
+  },
+});
+
+export default theme;
